15dfc440db04943017ec634f4c30ee8d51ecb2a2
Muthu <s.muthukhumar@gmail.com>
2019-12-03 16:46:47 +0800

    Initial commit

--- magic-bell/ViewController.swift
+++ magic-bell/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ivCenterImage: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     
     }
 
-
+    @IBAction func btnAsk(_ sender: Any) {
+        let arrayOfImages = [ #imageLiteral(resourceName: "ball1") ,#imageLiteral(resourceName: "ball3") ,#imageLiteral(resourceName: "ball2") ,#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball5")];
+        
+        let randomNumber = Int.random(in: 0...4);
+        print(randomNumber);
+        
+        ivCenterImage.image = arrayOfImages[randomNumber];
+        
+    }
+    
 }
 
