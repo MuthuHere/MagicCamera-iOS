2c8f5a06c7242401b778579d758dfd79a8766563
Muthu <s.muthukhumar@gmail.com>
2019-12-03 15:44:59 +0800

    Initial Commit

---
+++ magic-bell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic-bell
+//
+//  Created by Muthukumar on 03/12/2019.
+//  Copyright © 2019 Muthu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
